011983174fa931320d1a280d8497c708694252fc
jethoo <jeewanbhusal@gmail.com>
2020-07-23 23:00:14 -0400

    Initial Commit

---
+++ DemoTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoTableView
+//
+//  Created by jeewan bhusal on 23.7.2020.
+//  Copyright © 2020 jeewan bhusal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
